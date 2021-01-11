380b5a6ff2e20d0b2fa22ae7d943c73317bd93f3
mohab <mohab.mohammad3@gmail.com>
2021-01-12 00:17:46 +0200

    added step counter functionality

--- app/src/main/java/com/example/fitnessdoctor/StepCounterFragment.java
+++ app/src/main/java/com/example/fitnessdoctor/StepCounterFragment.java
@@ -1,5 +1,10 @@
 package com.example.fitnessdoctor;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,18 +19,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ekn.gruzer.gaugelibrary.ArcGauge;
 
 import java.text.DecimalFormat;
+import java.util.Set;
 
 
-public class StepCounterFragment extends Fragment {
+public class StepCounterFragment extends Fragment implements SensorEventListener {
     EditText etMeters;
     Button btnStart;
     ArcGauge arcGauge;
     NavController navController;
-    private static DecimalFormat df = new DecimalFormat("0");
+    SensorManager sensorManager;
+    boolean running = false;
+    double numberOfSteps = 0;
+    double previousNumberOfSteps = 0;
+    double meters=0;
 
     public StepCounterFragment() {
         // Required empty public constructor
@@ -52,18 +63,49 @@ public class StepCounterFragment extends Fragment {
         btnStart = view.findViewById(R.id.btn_start);
         arcGauge = view.findViewById(R.id.progress_circular);
         navController = Navigation.findNavController(view);
-
+        sensorManager = (SensorManager) this.getActivity().getSystemService(Context.SENSOR_SERVICE);
 
         btnStart.setOnClickListener(v -> {
-            double numberOfSteps;
-            double meters=0;
             try {
                 meters = Double.parseDouble(etMeters.getText().toString());
             }
             catch (NumberFormatException e){e.printStackTrace();}
             numberOfSteps = meters*1.31;
-            arcGauge.setMaxValue(Double.parseDouble(df.format(numberOfSteps)));
+            arcGauge.setMaxValue((int)numberOfSteps);
             arcGauge.setValue(0);
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        running = true;
+        Sensor counterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (counterSensor == null) {
+            Toast.makeText(getContext(), "No sensor was detected!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            sensorManager.registerListener(this, counterSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        running = false;
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (running){
+           // int currentSteps = (int)(numberOfSteps - previousNumberOfSteps);
+            arcGauge.setValue(event.values[0]);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
